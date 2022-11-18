6c8bba4a9be84a480e672b9168fa536ee6776726
chiziivictor <chizivwokoma@gmail.com>
2022-11-18 06:37:57 +0100

    feat: toggle visible team on click

--- src/components/Teams/Teams.tsx
+++ src/components/Teams/Teams.tsx
@@ -1,14 +1,20 @@
-import React from "react";
 import { TeamProps } from "@router/team";
+import React from "react";
 
-const Teams: React.FC<{ data: TeamProps }> = ({ data }) => {
+type Props = {
+  teams: TeamProps;
+  setId: React.Dispatch<React.SetStateAction<string | undefined>>;
+};
+
+const Teams: React.FC<Props> = ({ teams, setId }) => {
   return (
     <div className="col-span-1 border-r border-primary border-opacity-20 bg-secondary py-5 text-white">
       <ul className="flex flex-col items-center gap-4">
-        {data.map((team) => (
+        {teams?.map((team) => (
           <li
+            onClick={() => setId(team.id)}
             key={team.id}
-            className="flex h-10 w-10 items-center justify-center rounded-md border uppercase"
+            className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-md bg-white bg-opacity-20 font-bold uppercase transition-all hover:border"
           >
             {team.name.charAt(0)}
           </li>
